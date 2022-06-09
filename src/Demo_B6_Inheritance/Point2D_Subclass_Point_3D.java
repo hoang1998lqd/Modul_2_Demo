f638e8b7deab8916aef227a8f2de443c6bb6e21c
Regalite <hoang1998lqd@gmail.com>
2022-06-09 09:28:28 +0700

    add file points 2d và 3d

---
+++ src/Demo_B6_Inheritance/Point2D_Subclass_Point_3D.java
@@ -0,0 +1,2 @@
+package Demo_B6_Inheritance;public class Poin2D_Subclass_Point_3D {
+}
---
+++ src/Demo_B6_Inheritance/Points_2D.java
@@ -0,0 +1,44 @@
+package Demo_B6_Inheritance;
+
+public class Points {
+    private float x;
+    private float y;
+
+    public Points() {
+    }
+
+    public Points(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    public void setXY(float x, float y){
+        this 
+    }
+    public float getXY(){
+        return float [2];
+    }
+
+    @Override
+    public String toString() {
+        return "Points{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
